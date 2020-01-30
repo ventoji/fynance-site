456b44f6cd9ed779096bcaab2ddb6cec8dc73df7
Jorge Vento <vento.ji@gmail.com>
2020-01-30 09:55:57 +0100

    contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,39 +1,101 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Form, Button } from "react-bootstrap"
 
-const ContactForm = (props) => (
-    <div style={{ marginTop: "80px" }}>
-    <div className="row">
-      <div className="col-sm">
-        <h1> Contact</h1>
-      </div>
+const ContactForm = (props) => {
 
-      <div className="col-sm">
-        <Form>
+const [validated, setValidated] = useState(false);
+
+const [values, setValues] = useState({
+  email: '',
+  name: '',
+  subject: '',
+  textarea: '',
+})
+const {email, name,subject, textarea } = values;
+
+const handleChange = name => event => {
+  // HOF
+  setValues({...values, [name]: event.target.value})
+
+}
+
+const handleSubmit = e => {
+ // e.preventDefault();
+  const form = e.currentTarget;
+  //console.log(values);
+  //alert(values);
+  
+  if (form.checkValidity() === false) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+  setValidated(true);
+}
+ 
+   return (
+        <Form 
+          noValidate 
+          validated={validated} 
+          onSubmit={handleSubmit} 
+          method="POST" data-netlify="true"
+        >
           <Form.Group controlId="contactForm.Name">
             <Form.Label>Name</Form.Label>
-            <Form.Control type="text" placeholder="name" />
-          </Form.Group>
+            <Form.Control 
+              required 
+              type="text" 
+              placeholder="name" 
+              value={name}
+              onChange={handleChange('name')} 
+            />
+            <Form.Control.Feedback type="invalid">
+            Please provide a valid name.
+          </Form.Control.Feedback>
+            </Form.Group>
           <Form.Group controlId="contactForm.Email">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="name@example.com" />
-          </Form.Group>
+            <Form.Control 
+              required 
+              type="email" 
+              placeholder="name@example.com" 
+              value={email}
+              onChange={handleChange('email')} 
+              />
+            <Form.Control.Feedback type="invalid">
+            Please provide a valid Email address.
+          </Form.Control.Feedback>
+            </Form.Group>
           <Form.Group controlId="contactForm.Subject">
             <Form.Label>Subject</Form.Label>
-            <Form.Control type="text" placeholder="subject" />
-          </Form.Group>
+            <Form.Control 
+              required 
+              type="text" 
+              placeholder="subject"
+              value={subject}
+              onChange={handleChange('subject')}  
+              />
+            <Form.Control.Feedback type="invalid">
+            Please provide a subject.
+          </Form.Control.Feedback>
+            </Form.Group>
           <Form.Group controlId="contactForm.Message">
             <Form.Label>Example textarea</Form.Label>
-            <Form.Control as="textarea" rows="3" />
-          </Form.Group>
+            <Form.Control 
+              required 
+              as="textarea" 
+              rows="3" 
+              value={textarea}
+              onChange={handleChange('textarea')} 
+              />
+            <Form.Control.Feedback type="invalid">
+            Please provide a valid content.
+          </Form.Control.Feedback>
+            </Form.Group>
 
           <Button variant="primary" type="submit">
             Submit
           </Button>
         </Form>
-      </div>
-    </div>
-  </div>
     );
-
+}
 export default ContactForm;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,21 @@
 import React from "react"
 import PrimaryLayout from "../layouts/PrimaryLayout"
 import ContactForm from "../components/ContactForm"
+// import ContactForm2 from "../components/ContactForm2"
 
 const Contact = () => (
   <PrimaryLayout>
+    <div style={{ marginTop: "80px" }}>
+      <div className="row">
+        <div className="col-sm">
+          <h1> Contact</h1>
+        </div>
+        <div className="col-sm">
+          <ContactForm />
+        </div>
+      </div>
+    </div>
 
-    <ContactForm />
   </PrimaryLayout>
 )
 
